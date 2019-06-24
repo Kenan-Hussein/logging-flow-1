48a71309baa33843d0ff5421dc1f5fb646ba84fe
Kenan Hussein <in.ctrl.x@hotmail.com>
2019-06-24 17:18:25 +0300

    update logmessage code

--- src/Message/LogMessage.php
+++ src/Message/LogMessage.php
@@ -6,5 +6,15 @@ namespace App\Message;
 
 class LogMessage
 {
+    private $content;
 
+    public function __construct(string $content)
+    {
+        $this->content = $content;
+    }
+
+    public function getContent(): string
+    {
+        return $this->content;
+    }
 }
