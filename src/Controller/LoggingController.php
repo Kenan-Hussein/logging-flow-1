1b511508aa9d0912f517ab292fe8a3099a4515ac
Kenan Hussein <in.ctrl.x@hotmail.com>
2019-06-24 16:31:34 +0300

    make loggingController

---
+++ src/Controller/LoggingController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+
+class LoggingController extends AbstractController
+{
+    /**
+     * @Route("/logging", name="logging", methods={"POST"})
+     */
+    public function postLog()
+    {
+        return new JsonResponse(
+            [
+                'message' => 'yeaaah it is working',
+            ],
+            JsonResponse::HTTP_OK
+        );
+    }
+}
